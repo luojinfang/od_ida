63664cd8d922a944b38d6e2333cbd5a57d85adad
fc <fc@111.com>
2021-04-16 18:35:21 +0800

    。

---
+++ recored01/新建文件夹/消息 socket_libuv_发送_____.cpp
@@ -0,0 +1,79 @@
+线程 ID  地址       返回到      返回自      大小  注释                         方   
+204836                                                           
+       089FFA14 55F0DA6B 76D73500 54  ws2_32.76D73500            用户模块
+       089FFA68 55F0C1C5 55F0DA6B 2C  libuv.uv__send+9B          用户模块
+       089FFA94 55ED881B 55F0C1C5 28  libuv.uv__udp_send+95      用户模块
+       089FFABC 52AF6A5C 55ED881B 4C  libuv.uv_udp_send+6B       用户模块
+       089FFB08 52AFB499 52AF6A5C 5C  arksocket.52AF6A5C         用户模块
+       089FFB64 52AFB413 52AFB499 54  arksocket.52AFB499         用户模块
+       089FFBB8 52AFB124 52AFB413 50  arksocket.52AFB413         用户模块
+       089FFC08 52AF2E4B 52AFB124 18  arksocket.52AFB124         用户模块
+       089FFC20 52AF2602 52AF2E4B 20  arksocket.52AF2E4B         用户模块
+       089FFC40 52AF25B9 52AF2602 28  arksocket.52AF2602         用户模块
+       089FFC68 52AFA17F 52AF25B9 28  arksocket.52AF25B9         用户模块
+       089FFC90 52AF9FD2 52AFA17F 70  arksocket.52AFA17F         用户模块
+       089FFD00 56DF4302 52AF9FD2 2C  arksocket.52AF9FD2         用户模块
+       089FFD2C 56DF9E9A 56DF4302 2C  preloginlogic.56DF4302     用户模块
+       089FFD58 56DFCB24 56DF9E9A 74  preloginlogic.56DF9E9A     用户模块
+       089FFDCC 56DF79DF 56DFCB24 24  preloginlogic.56DFCB24     用户模块
+       089FFDF0 56DF7DFF 56DF79DF 24  preloginlogic.56DF79DF     用户模块
+       089FFE14 56DF7A27 56DF7DFF 20  preloginlogic.56DF7DFF     用户模块
+       089FFE34 56DF75F0 56DF7A27 24  preloginlogic.56DF7A27     用户模块
+       089FFE58 52AF159A 56DF75F0 C   preloginlogic.56DF75F0     用户模块
+       089FFE64 52AF5297 52AF159A 2C  arksocket.52AF159A         用户模块
+       089FFE90 55EDBD7B 52AF5297 3C  arksocket.52AF5297         用户模块
+       089FFECC 55EDAEB4 55EDBD7B 18  libuv.$LN50+13             用户模块	//55EDBD76                 call    j__uv_process_async_wakeup_req
+       089FFEE4 52AF5696 55EDAEB4 20  libuv.uv_run+74            用户模块	//55EDAEAF                 call    uv_process_reqs
+       089FFF04 55EF3370 52AF5696 28  arksocket.52AF5696         用户模块
+       089FFF2C 50E86CF2 55EF3370 48  libuv.uv__thread_start+70  系统模块
+       089FFF74 75FE0419 50E86CF2 10  ucrtbased.50E86CF2         系统模块
+       089FFF84 770566DD 75FE0419 5C  kernel32.75FE0419          系统模块
+       089FFFE0 770566AD 770566DD 10  ntdll.770566DD             系统模块
+       089FFFF0 00000000 770566AD     ntdll.770566AD             用户模块
+ 
+204836                                                                  
+       089FFCEC 55F0DA6B 76D73500 54  ws2_32.76D73500                   用户模块
+       089FFD40 55F0C1C5 55F0DA6B 2C  libuv.uv__send+9B                 用户模块
+       089FFD6C 55ED881B 55F0C1C5 28  libuv.uv__udp_send+95             用户模块
+       089FFD94 52AF6A5C 55ED881B 4C  libuv.uv_udp_send+6B              用户模块
+       089FFDE0 52AFB499 52AF6A5C 5C  arksocket.52AF6A5C                用户模块
+       089FFE3C 52AFB578 52AFB499 1C  arksocket.52AFB499                用户模块
+       089FFE58 52AF6B73 52AFB578 1C  arksocket.52AFB578                用户模块
+       089FFE74 55F0CD50 52AF6B73 1C  arksocket.52AF6B73                用户模块
+       089FFE90 55EDBD60 55F0CD50 3C  libuv.uv_process_udp_send_req+1E0 用户模块	//55F0CD4E                 call    ecx
+       089FFECC 55EDAEB4 55EDBD60 18  libuv.$LN49+14                    用户模块	//55EDBD5B                 call    j__uv_process_udp_send_req
+       089FFEE4 52AF5696 55EDAEB4 20  libuv.uv_run+74                   用户模块  	//55EDAEAF                 call    uv_process_reqs
+       089FFF04 55EF3370 52AF5696 28  arksocket.52AF5696                用户模块
+       089FFF2C 50E86CF2 55EF3370 48  libuv.uv__thread_start+70         系统模块
+       089FFF74 75FE0419 50E86CF2 10  ucrtbased.50E86CF2                系统模块
+       089FFF84 770566DD 75FE0419 5C  kernel32.75FE0419                 系统模块
+       089FFFE0 770566AD 770566DD 10  ntdll.770566DD                    系统模块
+       089FFFF0 00000000 770566AD     ntdll.770566AD                    用户模块
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
